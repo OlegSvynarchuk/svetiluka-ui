c7a1574db18ec6190b136431ab44795930515b3f
OlegSvynarchuk <zhelibon@gmail.com>
2021-07-06 19:20:13 +0200

    Changes for Heroku

---
+++ src/pages/searchpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from '../search/search.jsx'
+import Header from '../header/index.jsx'
+import Footer from '../footer/index.jsx'
+
+
+export default function SearchPage() {
+    return (
+        <div>
+            <Header></Header>
+            <Search /> 
+            <Footer></Footer>
+        </div>
+    )
+}
